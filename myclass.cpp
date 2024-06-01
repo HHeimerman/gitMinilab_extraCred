b3276c1c0e0f302c103bac66c5b133045f6b02b0
Huddy H <hheimerman@scu.edu>
2024-05-31 22:04:44 -0700

    Update hello.cpp, myclass.cpp, and myclass.h

--- myclass.cpp
+++ myclass.cpp
@@ -4,9 +4,13 @@ MyClass::MyClass() {
 }
 
 MyClass::MyClass(int x_) { 
-  x = x_; 
+  x = x_;
+  newX = x_*2; 
 }
 
 int MyClass::getX() { 
   return x; 
+}
+int MyClass::getNewX() { 
+  return newX; 
 }
